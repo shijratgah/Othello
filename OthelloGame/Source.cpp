9968021f1c583e64bc9cf8eae58973e5b203de76
takuya shiraga <takuya.shiraga@j-tech.jp>
2018-05-02 14:47:17 +0900

    裏返し処理完成

--- OthelloGame/Source.cpp
+++ OthelloGame/Source.cpp
@@ -6,64 +6,45 @@
 #define BLACK 1
 #define WHITE 2
 
-/*
-//ボード（左上を[0][0])
-int board[8][8] = 
-{
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,2,1,0,0,0 },
-	{ 0,0,0,1,2,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 },
-	{ 0,0,0,0,0,0,0,0 }
-};
-*/
-
 //ボード（左上を[0][0])
 int board[9][9] =
 {
-
-	{ 0,0,0,2,0,0,1,0,-1 },
-	{ 0,1,0,1,0,2,0,0,-1 },
-	{ 0,0,1,1,2,0,0,0,-1 },
-	{ 2,0,2,0,1,1,2,0,-1 },
-	{ 0,0,2,1,2,0,0,0,-1 },
-	{ 0,2,0,2,0,1,0,0,-1 },
-	{ 2,0,0,2,0,0,1,0,-1 },
-	{ 0,0,0,2,0,0,0,2,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
+	{ 0,0,0,2,1,0,0,0,-1 },
+	{ 0,0,0,1,2,0,0,0,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
+	{ 0,0,0,0,0,0,0,0,-1 },
 {-1,-1,-1,-1,-1,-1,-1,-1,-1}
-
 };
 
-
 //マスの状態の数
-static int NONE_NUMBER;  //空きマス
-static int BLACK_NUMBER = 2; //黒駒
-static int WHITE_NUMBER = 2; //白駒
-int FLAG = 1;//継続判定 1:続行 0:終了
-int Date[7];//裏返しの判定材料 自分がいる情報は無視
-static int sub_count = 0; //関数の呼び出し回数
-static int y, x;//座標
+static int NONE_NUMBER;			//空きマス
+static int BLACK_NUMBER = 2;	//黒駒
+static int WHITE_NUMBER = 2;	//白駒
+int FLAG = 1;					//継続判定 1:続行 0:終了
+int Date[7];					//裏返しの判定材料 自分がいる情報は無視
+bool Turn_FLAG = false;			//どこか1列でも裏返せるかの判定 1:可 0:不
+static int y, x;				//座標
 
-int turn = BLACK;			//1が先手、2が後手 
-int Player1, Player2;//プレイヤー
+int turn = BLACK;				//1がBLACK、2がWHITE 
+int Player1, Player2;			//プレイヤー
 
-void Disp(void); //画面の更新・駒数の再描画
-void CanPut(void); //置けるかの処理　パス判定　両方駒は残っているけど置けない
-void TurnOver(int Date[7],int place,int j); //裏返す処理 turn処理
-int FinishCheck(int BLACK_NUMBER,int WHITE_NUMBER,int NONE_NUMBER); //ゲームが続行できるかの判定 1は続行 0は終了
+void Disp(void);														//画面の更新・駒数の再描画
+void CanPut(void);														//駒が置けるかの処理
+int CanPut_sub(int place);												//CanPutに入力した位置に駒が置いてるかを返す 1:ない 0:ある
+void PassCheck(void);													//パス判定　両方駒は残っているけど置けない時の処理
+void TurnOver(int Date[7],int place,int j,int sub_count);				//裏返す処理 turn処理
+int FinishCheck(int BLACK_NUMBER,int WHITE_NUMBER,int NONE_NUMBER);		//ゲームが続行できるかの判定 1は続行 0は終了
 /*---------------------------------MAIN----------------------------------------------*/
 int main(void) {
-	//継続判定
-	while(FLAG){
+	while(FLAG){//継続判定
 		Disp();
 		CanPut();
 	}
-	//勝利判定
-	if (BLACK_NUMBER > WHITE_NUMBER) {
-
+	if (BLACK_NUMBER > WHITE_NUMBER) {//勝利判定
 		printf("黒の勝利!!\n");
 	}
 	else if (WHITE_NUMBER > BLACK_NUMBER) {
@@ -77,25 +58,29 @@ int main(void) {
 
 //画面の更新・駒数の再描画
 void Disp() {
-	NONE_NUMBER = 0;  //空きマス
-	BLACK_NUMBER = 0; //黒駒
-	WHITE_NUMBER = 0; //白駒
-	//画面クリア
-	//system("cls");
-	//行を描画
+	NONE_NUMBER = 0;	//空きマス
+	BLACK_NUMBER = 0;	//黒駒
+	WHITE_NUMBER = 0;	//白駒
+	system("cls");		//画面クリア
+	for (int i = 0; i < 9; i++) {		//周りに-1を入れて初期化
+		board[i][8] = -1;
+		for (int j = 0; j < 9; j++) {
+			board[8][i] = -1;
+		}
+	}
 	printf(" ");
-	for (int i = 1; i <= 8; i++) {
+	for (int i = 1; i <= 8; i++) {		//行を描画
 		printf("%2d", i);
 	}
 	printf("\n");
 	for (int i = 0; i < 8; i++) {
-		//列を描写
-		printf("%d", ((i + 1) * 10));
+		
+		printf("%d", ((i + 1) * 10));	//列を描写
 		for (int j = 0; j < 8; j++) {
 			switch (board[i][j]) {
-			case NONE: printf("□"); NONE_NUMBER++; break; //駒が無かったら'□'			
-			case BLACK: printf("○"); BLACK_NUMBER++; break;//1(黒)が入っていたら'○'	
-			case WHITE: printf("●"); WHITE_NUMBER++; break;//2(白)が入っていたら'●'	
+			case NONE: printf("□"); NONE_NUMBER++; break;			//駒が無かったら'□'			
+			case BLACK: printf("○"); BLACK_NUMBER++; break;		//1(黒)が入っていたら'○'	
+			case WHITE: printf("●"); WHITE_NUMBER++; break;		//2(白)が入っていたら'●'	
 			default: break;
 			}
 		}
@@ -103,17 +88,14 @@ void Disp() {
 	}
 	printf("○黒駒の数:%2d\n", BLACK_NUMBER);
 	printf("●白駒の数:%2d\n", WHITE_NUMBER);
-	//継続判断
-	FLAG = FinishCheck(BLACK_NUMBER, WHITE_NUMBER, NONE_NUMBER);
+	FLAG = FinishCheck(BLACK_NUMBER, WHITE_NUMBER, NONE_NUMBER);	//継続判断
 }
 
-
-//置けるかの処理
-void CanPut() {
-	char pl[3];//外部の文字を格納
-	int place;//char型をキャストするための整数
-	//継続可能のときだけ表示する
-	if (FLAG) {
+//駒が置けるかの処理
+void CanPut() { 
+	char pl[3];			//外部の文字を格納
+	int place;			//char型をキャストするための整数
+	if (FLAG) {			//継続可能のときだけ表示する
 		printf("---------------------------\n");
 		printf("場所を選択して下さい\n");
 		printf("例 40 + 2 -> 42\n");
@@ -129,16 +111,18 @@ void CanPut() {
 	else {
 		return;
 	}
-	while (1) {
-		//置く場所の選択
-		scanf_s("%s", pl, 3);
-		//数字以外が入っているかチェック
-		if (isdigit(*pl)) {
-			//文字列を整数型にキャスト
-			place = atoi(pl);
-			//置ける範囲チェック
-			if (place >= 11 && place <= 88 && (place % 10) != 9 && (place % 10) != 0) {
-				break;
+	while (1) {					//入力処理
+		scanf_s("%s", pl, 3);	//置く場所の選択
+		if (isdigit(*pl)) {		//数字以外が入っているかチェック
+			place = atoi(pl);	//文字列を整数型にキャスト
+			if (place >= 11 && place <= 88 && (place % 10) != 9 && (place % 10) != 0) {	//置ける範囲チェック
+				if (CanPut_sub(place)) {	//すでに駒が置いてあるか
+					break;
+				}
+				else {
+					printf("\n入力した場所は無効です\n");
+					printf("場所:");
+				}
 			}
 			else {
 				printf("\n入力した文字は無効です\n");
@@ -150,18 +134,19 @@ void CanPut() {
 			printf("場所:");
 		}
 	}
-	
-	//渡された整数を分割
-	y = place / 10;
+}
+
+//CanPutに入力した位置に駒が置いてるかを返す 1:ない 0:ある
+int CanPut_sub(int place) {
+	int sub_count = 1;	//TurnOver関数の呼び出し回数
+	y = place / 10;		//渡された整数を分割
 	x = place % 10;
 	y--;
 	x--;
-	
-	//駒がすでにある場合は置けない
-	if (board[y][x] == 0) {
-		int j;		//何回回るかの値
-		//1:上ok
-		j = y - 1;
+	if (board[y][x] == 0) {		//駒がすでにある場合は置けない
+		int j;				//何回回るかの値
+							//入力された位置からの全方位のデータを関数TurnOverに値を渡している				
+		j = y - 1;								//1:上
 		for (int i = 0; i <= j; i++) {
 			if (board[--y][x]) {
 				Date[i] = board[y][x];
@@ -170,11 +155,9 @@ void CanPut() {
 				Date[i] = 0;
 				break;
 			}
-			
 		}
-		TurnOver(Date,place,j);
-		//2:右上
-		j = y - 1;
+		TurnOver(Date, place, j, sub_count++);
+		j = y - 1;								//2:右上
 		for (int i = 0; i <= j; i++) {
 			if (board[--y][++x] != -1) {
 				Date[i] = board[y][x];
@@ -183,15 +166,13 @@ void CanPut() {
 				break;
 			}
 		}
-		TurnOver(Date, place, j);
-		//3:右ok
-		j = 6;
+		TurnOver(Date, place, j, sub_count++);
+		j = 6;									//3:右
 		for (int i = 0; i <= j; i++) {
 			Date[i] = board[y][++x];
 		}
-		TurnOver(Date, place, j);
-		//4:右下
-		j = 6;
+		TurnOver(Date, place, j, sub_count++);
+		j = 6;									//4:右下
 		for (int i = 0; i <= j; i++) {
 			if (board[++y][++x] != -1) {
 				Date[i] = board[y][x];
@@ -200,15 +181,13 @@ void CanPut() {
 				break;
 			}
 		}
-		TurnOver(Date, place, j);
-		//5:下ok
-		j = 7 - y;
+		TurnOver(Date, place, j, sub_count++);
+		j = 7 - y;								//5:下
 		for (int i = 0; i <= j; i++) {
 			Date[i] = board[++y][x];
 		}
-		TurnOver(Date, place, j);
-		//6:左下
-		j = x - 1;
+		TurnOver(Date, place, j, sub_count++);
+		j = x - 1;								//6:左下
 		for (int i = 0; i <= j; i++) {
 			if (board[++y][--x] != -1) {
 				Date[i] = board[y][x];
@@ -217,15 +196,13 @@ void CanPut() {
 				break;
 			}
 		}
-		TurnOver(Date, place, j);
-		//7:左ok
-		j = x - 1;
+		TurnOver(Date, place, j, sub_count++);
+		j = x - 1;								//7:左							
 		for (int i = 0; i <= j; i++) {
 			Date[i] = board[y][--x];
 		}
-		TurnOver(Date, place, j);
-		//8:左上
-		j = x - 1;
+		TurnOver(Date, place, j, sub_count++);
+		j = x - 1;								//8:左上
 		for (int i = 0; i <= j; i++) {
 			if (board[--y][--x] != -1) {
 				Date[i] = board[y][x];
@@ -234,168 +211,156 @@ void CanPut() {
 				break;
 			}
 		}
-		TurnOver(Date, place, j);
+		TurnOver(Date, place, j, sub_count++);
+		return 1;
 	}
 	else {
-		printf("置けません\n");
+		return 0;
 	}
 }
 
+//パス判定　両方駒は残っているけど置けない時の処理
+void PassCheck() {
 
+}
 
 //裏返す処理
-void TurnOver(int Date[7],int place, int j){
-	int TurnFlag = 0; //ひっくり返すかの判定 1:続行 0:終了
-	int  count = 0;
-	sub_count++;
-
-	//渡された整数を分割
-	y = place / 10;
+void TurnOver(int Date[7],int place, int j,int sub_count){
+	int count_FLAG;				//その1列を裏返せるかの判定　1:可 0:不
+	int  count;					//駒を裏返す回数
+	count = count_FLAG = 0;		//初期化
+	y = place / 10;				//渡された整数を分割
 	x = place % 10;
 	y--;
 	x--;
-	printf("count: %d\n", sub_count);
-	for (int i = 0; i <= j; i++) {
-		printf("%d", Date[i]);
-	}
-	printf("\n");
-	printf("%d", j);
-	printf("\n");
-
-
-
-
+	
 	//何回裏返せるかの判定
-	if (Date[0] <= 0 || Date[0] == turn) {
-		printf("返せない\n\n");
+	if (Date[0] <= 0 || Date[0] == turn) {			//1列目の0番目の要素が0,-1,自分のターンの色でないときの処理
 	}
 	else {
-		TurnFlag = 1;
-		printf("返せるmaybe\n");
-		for (int i = 0; i <= j; i++) {
-			if (turn == BLACK) {
-				if (Date[i] == WHITE) {
+		if (turn == BLACK) {						//黒番の処理
+			for (int i = 0; i <= j; i++) {
+				if (Date[i] == WHITE) {				//1列目のi番目の要素が白の間回る
 					count++;
-					printf("//////////////BLACK///////////////////\n");
-					printf("%d\n",count);
-					printf("/////////////////////////////////\n");
 				}
-				else {
+				else if (Date[i] == BLACK) {
+					count_FLAG = 1;					//白駒を黒駒で挟めた時の判定
 					break;
 				}
-			}
-			else if (turn == WHITE) {
-				if (Date[i] == BLACK) {
-					count++;
-					printf("////////////////WHITE/////////////////\n");
-					printf("%d\n", count);
-					printf("/////////////////////////////////\n");
-				}
-				else {
+				else if (Date[i] <= 0) {
 					break;
 				}
-			}
-			else if (Date[++i] == 0) {
-				break;
 			}
 		}
-		printf("%d\n",j);
-
-
+		else if (turn == WHITE) {					//白番の処理
+			for (int i = 0; i <= j; i++) {
+				if (Date[i] == BLACK) {				//1列目のi番目の要素が黒の間回る
+					count++;
+				}
+				else if (Date[i] == WHITE) {
+					count_FLAG = 1;					//白駒を黒駒で挟めた時の判定
+					break;
+				}
+				else if (Date[i] <= 0) {
+					break;
+				}
+			}
+		}
 	}
 
-
 	//裏返しの処理
-	switch (sub_count) {
-	case 1:
-		//上
-		for (int i = 1; i <= count; i++) {
-			board[y - i][x] = turn;
+	switch (sub_count) {							//sub_countの数字によって処理する　例）1,上　4,右下
+	case 1:											//上
+		if (count_FLAG) {							
+			for (int i = 1; i <= count; i++) {		//countの回数分だけ回る
+				board[y - i][x] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//右上
-	case 2:
-		for (int i = 1; i <= count; i++) {
-			board[y - i][x + i] = turn;
+	case 2:											//右上
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y - i][x + i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//右
-	case 3:
-		for (int i = 1; i <= count; i++) {
-			board[y][x + i] = turn;
+	case 3:											//右
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y][x + i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//右下
-	case 4:
-		for (int i = 1; i <= count; i++) {
-			board[y + i][x + i] = turn;
+	case 4:											//右下
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y + i][x + i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//下
-	case 5:
-		for (int i = 1; i <= count; i++) {
-			board[y + i][x] = turn;
+	case 5:											//下
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y + i][x] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//左下
-	case 6:
-		for (int i = 1; i <= count; i++) {
-			board[y + i][x - i] = turn;
+	case 6:											//左下
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y + i][x - i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//左
-	case 7:
-		for (int i = 1; i <= count; i++) {
-			board[y][x - i] = turn;
+	case 7:											//左
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y][x - i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
-		//左上
-	case 8:
-		for (int i = 1; i <= count; i++) {
-			board[y - i][x - i] = turn;
-		}
-		sub_count = 0;
-		if (TurnFlag) {
-			printf("裏返せれません\n");
-		}
-		if (turn == BLACK) {
-			turn = WHITE;//ホワイトは後攻
-		}
-		else {
-			turn = BLACK;//ブラックは先攻
+	case 8:											//左上
+		if (count_FLAG) {
+			for (int i = 1; i <= count; i++) {
+				board[y - i][x - i] = turn;
+			}
+			Turn_FLAG = true;
 		}
 		break;
 	default:
 		break;
 	}
 
-	
-
-	/*
-	//ターン変更処理 8回とも置けなかったら置けませんでした出力
-	if (sub_count == 8) {
-		if (turn == BLACK) {
-			turn = WHITE;//ホワイトは後攻
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if (sub_count == 8) {					//ターンチェンジ
+		sub_count = 0;
+		if (Turn_FLAG != true) {			//どの方位も裏返らない
+			printf("置けません\n");
 		}
 		else {
-			turn = BLACK;//ブラックは先攻
+			board[y][x] = turn;				//入力した場所に駒を置く
+			if (turn == BLACK) {
+				turn = WHITE;				//ホワイトは後攻
+			}
+			else {
+				turn = BLACK;				//ブラックは先攻
+			}
 		}
+		Turn_FLAG = false;
 	}
-	*/
-
-
-
-
-	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-
-
-
 
 //ゲームが続行できるかの判定 1は続行 0は終了
 int FinishCheck(int BLACK_NUMBER,int WHITE_NUMBER, int NONE_NUMBER) {
-	//どちらかの色が一色になってないか
-	if (BLACK_NUMBER == 0) {
+	if (BLACK_NUMBER == 0) {										//どちらかの色が一色になってないか
 		printf("すべての駒が白になりました。\n");
 		FLAG = 0;
 	}
@@ -403,9 +368,8 @@ int FinishCheck(int BLACK_NUMBER,int WHITE_NUMBER, int NONE_NUMBER) {
 		printf("すべての駒が黒になりました。\n");
 		FLAG = 0;
 	}
-	else if (NONE_NUMBER == 0) {//盤面が埋まっているか
+	else if (NONE_NUMBER == 0) {									//盤面が埋まっているか
 		FLAG = 0;
 	}
-	
 	return FLAG;
 }
